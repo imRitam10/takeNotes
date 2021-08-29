7c671325ecf44afef6a5a2264884dd2a39fe2826
Ritam Bhadra <imritam29@gmail.com>
2021-08-30 00:48:35 +0530

    Add: backend reg api

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,31 @@ const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRouts = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running...");
+// });
 
 app.get("/api/notes", (req, res) => {
   res.send(notes);
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.send(note);
-});
+// app.get("/api/notes/:id", (req, res) => {
+//   const note = notes.find((n) => n._id === req.params.id);
+//   res.send(note);
+// });
+
+app.use("/api/users", userRouts);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
